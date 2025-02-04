6371d2de377fa2eac0731fb1b4d1ccfe35f80630
Vanessa Roque <viroque19@gmail.com>
2025-02-04 04:03:35 +0000

    add light script

--- backend/scripts/log_data.py
+++ backend/scripts/log_data.py
@@ -2,29 +2,34 @@
 import sqlite3
 import time
 import os
+import pytz
 import random  # Replace with actual sensor reading
+from datetime import datetime
+
 
 script_dr = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(script_dr, "../data/sleepData.db")
+db_path = os.path.join(script_dr, "../instance/sleeptracker.db")
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
+tz_LA = pytz.timezone("America/Los_Angeles")
+
 
 lux_max = 1300
 
 
 def log_data():
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = time.strftime("%H:%M:%S")
     motion = random.randint(0, 1)  # Replace with actual light sensor reading
     temperature = round(random.uniform(20, 30), 2)  # Replace with actual temp sensor
     light = str(random.randint(0, lux_max))
-
+    date = datetime.now(tz_LA).strftime("%a")
     cursor.execute(
-        "INSERT INTO sleep_data (timestamp, light, temperature, motion) VALUES (?, ?, ?, ?)",
-        (timestamp, light, temperature, motion),
+        "INSERT INTO sleep_data (date, timestamp, light, temperature, motion) VALUES (?, ?, ?, ?, ?)",
+        (date, timestamp, light, temperature, motion),
     )
     conn.commit()
     print(
-        f"Logged: {timestamp} | Light: {light} | Temp: {temperature}°C | Motion: {motion}"
+        f"Logged: {date} | {timestamp} | Light: {light} | Temp: {temperature}°C | Motion: {motion}"
     )
 
 
