ae56c16b577d94d0bf34b2bf6144034c4b018e32
Vanessa Roque <viroque19@gmail.com>
2025-03-03 06:52:20 +0000

    update chart

--- backend/scripts/graph.py
+++ backend/scripts/graph.py
@@ -6,6 +6,7 @@ import datetime
 from mpl_toolkits.axes_grid1 import host_subplot
 import pandas as pd
 import matplotlib.dates as mdates
+import pytz
 
 from query import get_sensor_data
 
@@ -16,7 +17,8 @@ TEMP_COLOR = "#3b7ec4"
 MOTION_COLOR = "#f3590c"
 LIGHT_COLOR = "#484cb7"
 
-PLOT_PATH = "../charts/"
+PLOT_PATH = "../../static/charts/"
+tz_LA = pytz.timezone("America/Los_Angeles")
 
 
 def analyze_data(df):
@@ -69,15 +71,16 @@ def analyze_data(df):
 
 def main():
     df = get_sensor_data()
-    plt.rcParams["figure.figsize"] = (16, 6)
+    plt.rcParams["figure.figsize"] = (26, 12)
+    plt.tight_layout()
     host = host_subplot(111)
     plt.subplots_adjust(right=0.75)
     fig = plt.gcf()
-    fig.patch.set_facecolor(AXES_COLOR)
-    host.set_facecolor(AXES_COLOR)
+    fig.patch.set_facecolor(FACE_COLOR)
+    host.set_facecolor(FACE_COLOR)
     ax1 = host.twinx()  # temp
     ax2 = host.twinx()  # motion
-    ax2.spines["right"].set_position(("outward", 50))  # Offset third axis
+    ax2.spines["right"].set_position(("outward", 65))  # Offset third axis
     # df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.strftime("%H:%M")
     # time_labels = df_hourly["hour_bin"]
 
@@ -115,20 +118,30 @@ def main():
     ax1.set_xticklabels(new_tick_labels, fontsize=12)
 
     # Set Labels & Ranges
-    host.set_xlabel("Time", color=FACE_COLOR, fontweight="bold")
-    host.set(ylabel="Light Intensity (Lx)", ylim=(light_min, light_max))
-    ax1.set(ylabel="Temperature (°C)", ylim=(temp_min, temp_max))
-    ax2.set(ylabel="Motion (Boolean)", ylim=(0, 1))
+    host.set_xlabel("Time", color=AXES_COLOR, fontweight="bold", fontsize=14)
+    host.set_ylabel("Light Intensity (Lx)", fontsize=14)
+    host.set_ylim(light_min, light_max)
+
+    ax1.set_ylabel("Temperature (°C)", fontsize=14)
+    ax1.set_ylim(temp_min, temp_max)
+
+    ax2.set_ylabel("Motion (Boolean)", fontsize=14)
+    ax2.set_ylim(0, 1)
+
+    # Optionally, adjust tick label sizes
+    host.tick_params(axis="both", labelsize=12)
+    ax1.tick_params(axis="both", labelsize=12)
+    ax2.tick_params(axis="both", labelsize=12)
 
     host.legend(loc="best")
-    host.tick_params(axis="x", colors=FACE_COLOR, labelsize=14, rotation=0)
+    host.tick_params(axis="x", colors=AXES_COLOR, labelsize=14, rotation=0)
     host.tick_params(axis="y", colors=LIGHT_COLOR)
     ax1.tick_params(axis="y", colors=TEMP_COLOR)
     ax2.tick_params(axis="y", colors=MOTION_COLOR)
     host.yaxis.label.set_color(LIGHT_COLOR)
     ax1.yaxis.label.set_color(TEMP_COLOR)
     ax2.yaxis.label.set_color(MOTION_COLOR)
-    file_name = datetime.datetime.now().strftime("%Y-%m-%d")
+    file_name = datetime.datetime.now(tz_LA).strftime("%Y-%m-%d")
     plt.savefig(f"{PLOT_PATH+file_name}.png")
     print(f"Plot saved as {PLOT_PATH+file_name}.png")
     subprocess.run(["code", f"{PLOT_PATH+file_name}.png"])
